5d54a7eb92abec82312814536458a340754ed4f7
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2014-05-24 19:58:36 +0200

    Update main.cpp

--- 2.-Prime-Generator/main.cpp
+++ 2.-Prime-Generator/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+void generatePrimeBetween(int num1, int num2){
+ cout << "TODO: Generate Prime numbers between " << num1 << " and "
+ 	<< num2 << endl; 
+}
+
 int main() {
+	// your code goes here
+	int noOfTestCases;
+	scanf("%d", &noOfTestCases);
+	for (int i=0; i<noOfTestCases; ++i){
+		int num1, num2;
+		scanf("%d %d",&num1,&num2);
+		generatePrimeBetween(num1, num2);
+	}
 	return 0;
 }
