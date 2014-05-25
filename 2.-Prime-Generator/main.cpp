d0cc0c27fc500e62cda2e7c3da3def9f7355c727
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2014-05-25 15:51:01 +0200

    Create main.cpp

---
+++ 2.-Prime-Generator/main.cpp
@@ -0,0 +1,46 @@
+int prime[10000],primes;
+
+void init_primes(){
+     static bool isprime[32000];
+     int i,j;
+     for(i=2;i<32000;i++) isprime[i]=true;
+     for(i=2;i*i < 32000;i++)if(isprime[i])
+     {
+         for(j=2*i;j<32000;j+=i) isprime[j]=false;        
+     }
+     primes=0;
+     for(i=2;i<32000;i++)
+        if(isprime[i]) 
+            prime[primes++]=i;
+}
+
+bool seive[100001];
+
+void find_seive(int lo, int hi){
+   int i,j,p,n;
+   for(i=0; i<=hi-lo; i++) 
+       seive[i]=true;
+   for(j=0;j<=primes;j++){
+       p=prime[j];
+       if(p*p > hi) break; //stop when the max prime is reached
+       n = (lo/p)*p;
+       if(n<lo) n+=p;
+       if(n==p) n+=p;
+       for(;n<=hi; n+=p) 
+          seive[n-lo]=false;         
+   }  
+}
+
+int main(){
+	// your code goes here
+	int noOfTestCases;
+	scanf("%d", &noOfTestCases);
+	int num1; //smaller number
+	int num2; // larger number
+	while (noOfTestCases--) {
+		scanf("%d %d",&num1,&num2);
+		generatePrimeBetween(num1, num2);
+		cout << endl;
+	}
+	return 0;
+}
