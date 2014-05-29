93a20f4feb2a7c72393da5cb3f022d685f337f78
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2014-05-29 14:29:29 +0200

    Accepted! :sweat_smile:

--- 5.-The-Next-Palindrome/main.cpp
+++ 5.-The-Next-Palindrome/main.cpp
@@ -1,48 +1,73 @@
 #include <iostream>
-#include <stdio.h>
+#include <algorithm>
 #include <cstring>
-#include <stdlib.h>
-
+ 
 using namespace std;
-
-//function to reverse a string
-void reverse(char s[]) {
-	int c, i , j;
-	for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
-		c = s[i];
-		s[i] = s[j];
-		s[j] = c;
-	}
-	return;
-}
-bool isPlaindrome(char * a){
-	char b[strlen(a)+1];
-	strcpy(b,a);
-	reverse(a);
-	if(strcmp(a, b) == 0) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-char * nextPlaindrome(char * longDigitNumber){
-	int length = strlen(longDigitNumber);
-	char* pChar;
-	if(length % 2 != 0) {
-		*pChar = malloc(sizeof(char));
-		pChar = &longDigitNumber[int(length/2)];
+ 
+void nextPlaindrome(char * num){
+	int length = strlen(num);
+	char originalNum[length];
+	strcpy(originalNum, num);
+	int left, right;
+	if(length % 2) { // odd
+		left = length/2;
+		right = length/2;
 	}
 	else {
-		*pChar = malloc(sizeof(char)+1);
-		
-		pChar = &longDigitNumber[int(length/2)-1] ;
-		strncat (pChar, &longDigitNumber[int(length/2)], 2);
+	left = length/2-1;
+	right = length/2;
 	}
-	return pChar;
-	
+	while(left >= 0 && right < length){
+		if (num[left] != num[right]){
+			if ( num[left] > num[right]){
+				  num[right] = num[left]; //if 2313 => 2333 means greater
+				}
+				else{	
+					num[right] = num[left] ; //if 2133 => 2113 // always gets lesser
+				}
+		}
+		left--; right++;
+	}
+	if (strcmp(originalNum, num) < 0) //originalnum is less than the present number
+			cout << num << endl;
+	else{ //2112 has come
+		if(length % 2) { // odd
+		left = length/2;
+		right = length/2;
+		}
+		else {
+		left = length/2-1;
+		right = length/2;
+		}
+		while(left >= 0 && right < length){
+		if (num[left] != num[right]){
+			throw 1; //cant be ! its a error
+		}
+		if (num[left] != '9' ) {
+					num[left] = num[right] = num[left] + 1; //2112 => 2222 //808 => 818
+					for ( int i=left + 1; i<right ; i++ ) num[i] = '0' ; //stay lesser! 
+					//if 833338 is changed to 843348 => change to 840048
+					break ; //stop the while or else 919 will come !
+				}
+		left--; right++;
+		}
+		if ( strcmp(originalNum, num) < 0 ) cout << num << endl; 
+			else{ //9's left to do
+				if ( num[0] == '9' ){ //99 => 101
+					cout << '1' ;
+					for ( int i=0 ; i<length-1 ; i++ )cout << '0' ;
+					cout <<'1' ;
+ 
+				}
+				else{ // 1991 must send 2002
+					cout << (char)(num[0]+1) ; //2
+					for ( int i=0 ; i<length-2 ; i++ ) cout << '0' << endl;
+					cout << (char)(num[0] + 1) ; //2
+				}
+				cout << endl;
+			}
+	}
 }
-
 
 
 int main() {
@@ -51,7 +76,7 @@ int main() {
 	while(noOfCases--){
 		char longDigitNumber[1000001];
 		scanf("%s",&longDigitNumber);
-		cout << nextPlaindrome(longDigitNumber) << endl;;
+		nextPlaindrome(longDigitNumber);
 	}
-	return 0;
+return 0;
 }
