5f38968db98a9ba0410b45c354a8808d83be4059
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2014-06-09 11:54:46 +0200

    Update main.py

--- 24.-Small-factorials/main.py
+++ 24.-Small-factorials/main.py
@@ -3,8 +3,8 @@ cache = {}
 
 #recursive implementation
 def fact(n):
-    if n in memo:
-        return memo[n]
+    if n in cache:
+        return cache[n]
     if n == 0:
         return 1
     else:
